0f741013722cef5c46fb4ee84595246f0305364d
John Martinez <jmartinez729@gmail.com>
2018-08-20 16:49:31 +0000

    automatically backed up by learn

--- lib/move.rb
+++ lib/move.rb
@@ -6,6 +6,6 @@ def display_board(board)
   puts " #{board[6]} | #{board[7]} | #{board[8]} "
 end
 
-def input_to_index
-  input = (input - 1).to_i
+def input_to_index(user_input)
+ user_input = (user_input - 1).to_i
 end 
